894128386b6bcbfa3bc334af571698b7d4ef9490
Mike88002 <miky882002@gmail.com>
2024-11-19 16:04:34 +0100

    modifiche

--- script.js
+++ script.js
@@ -1,70 +1,123 @@
 let tot = document.querySelector('#tot');
 let btnShow = document.querySelector('#btnShow');
+let table = document.querySelector('.table');
+let tbody = document.querySelector('tbody');
+let inputName = document.querySelector('#inputName');
+let inputNumber = document.querySelector('#inputNumber');
+let inputDelete = document.querySelector('#inputDelete');
+let btnAdd = document.querySelector('#btnAdd');
+let btnDelete = document.querySelector('#btnDelete');
+let isHidden = false;
+
+// Modal e Toast
+let modalEdit = new bootstrap.Modal(document.getElementById('modalEdit'));
+let formEdit = document.getElementById('formEdit');
+let editName = document.getElementById('editName');
+let editNumber = document.getElementById('editNumber');
+let contattoSelezionato = null;
 
 let rubrica = {
     'contacts': [
-        {'nome': 'Michele', 'numero': 3331111111},
-        {'nome': 'Matteo', 'numero': 3332222222},
-        {'nome': 'Luca', 'numero': 3333333333},
-        {'nome': 'Davide', 'numero': 3334444444}
+        { 'nome': 'Michele', 'numero': 3331111111 },
+        { 'nome': 'Matteo', 'numero': 3332222222 },
+        { 'nome': 'Luca', 'numero': 3333333333 },
+        { 'nome': 'Davide', 'numero': 3334444444 }
     ],
-    'totContacts': function() {
+    'totContacts': function () {
         tot.innerHTML = this.contacts.length;
     },
-    'showContacts': function() {
-        // Reset del corpo della tabella
+    'showContacts': function () {
         tbody.innerHTML = '';
-
-        // creare le righe della tabella con i contatti
-        this.contacts.forEach((contact) => {
+        this.contacts.forEach(contact => {
             let tr = document.createElement('tr');
             tr.innerHTML = `
-                <td scope="row">${contact.nome}</td>
+                <td>${contact.nome}</td>
                 <td>${contact.numero}</td>
+                <td>
+                    <button class="btn btn-warning btn-sm me-2" onclick="apriModifica('${contact.nome}')">Modifica</button>
+                </td>
             `;
             tbody.appendChild(tr);
-        })
-
+        });
         this.totContacts();
     },
-    'addContact': function(nameContact, numberContact) {
-        this.contacts.push({'nome': nameContact, 'numero': numberContact});
+    'addContact': function (nameContact, numberContact) {
+        this.contacts.push({ 'nome': nameContact, 'numero': numberContact });
+    }
+};
+
+// Funzioni per il modal
+function apriModifica(nome) {
+    contattoSelezionato = rubrica.contacts.find(contact => contact.nome === nome);
+    if (contattoSelezionato) {
+        editName.value = contattoSelezionato.nome;
+        editNumber.value = contattoSelezionato.numero;
+        modalEdit.show();
     }
 }
 
-let table = document.querySelector('.table');
-let tbody = document.querySelector('tbody');
-let isHidden = false;
-
-rubrica.showContacts();
-
-rubrica.totContacts();
-
-btnShow.addEventListener('click', () => {
-    if (!isHidden) {
-        table.classList.add('d-none');
-        btnShow.innerHTML = `Mostra <i class="bi bi-eye-fill"></i>`;
-        isHidden = true;        
-    } else {
-        table.classList.remove('d-none');
-        btnShow.innerHTML = `Nascondi <i class="bi bi-eye-slash-fill"></i>`;  
-        isHidden = false;
+formEdit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (contattoSelezionato) {
+        contattoSelezionato.nome = editName.value;
+        contattoSelezionato.numero = editNumber.value;
+        rubrica.showContacts();
+        mostraToast('Modifica avvenuta');
+        modalEdit.hide();
     }
-})
+});
 
-let inputName = document.querySelector('#inputName');
-let inputNumber = document.querySelector('#inputNumber');
-let btnAdd = document.querySelector('#btnAdd');
-let btnDelete = document.querySelector('#btnDelete');
+// Funzione Toast
+function mostraToast(messaggio) {
+    const toast = document.createElement('div');
+    toast.className = 'toast align-items-center text-bg-success border-0';
+    toast.setAttribute('role', 'alert');
+    toast.setAttribute('aria-live', 'assertive');
+    toast.setAttribute('aria-atomic', 'true');
+    toast.innerHTML = `
+        <div class="d-flex">
+            <div class="toast-body">${messaggio}</div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+        </div>
+    `;
+    document.getElementById('toastContainer').appendChild(toast);
+    const bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+    toast.addEventListener('hidden.bs.toast', () => toast.remove());
+}
 
+// Eventi
 btnAdd.addEventListener('click', () => {
     rubrica.addContact(inputName.value, inputNumber.value);
-    console.log(rubrica.contacts);
     rubrica.showContacts();
-})
+    mostraToast('Contatto aggiunto');
+    inputName.value = '';
+    inputNumber.value = '';
+});
 
 btnDelete.addEventListener('click', () => {
-    rubrica.contacts = rubrica.contacts.filter((contact) => contact.nome!== inputName.value);
-    rubrica.showContacts();
-})
+    const nomeEliminato = inputDelete.value;
+    const contactsFiltrati = rubrica.contacts.filter(contact => contact.nome !== nomeEliminato);
+    if (contactsFiltrati.length < rubrica.contacts.length) {
+        rubrica.contacts = contactsFiltrati;
+        rubrica.showContacts();
+        mostraToast(`Contatto "${nomeEliminato}" Eliminato`);
+    } else {
+        mostraToast(`Nessun contatto "${nomeEliminato}"`);
+    }
+});
+
+btnShow.addEventListener('click', () => {
+    isHidden = !isHidden;
+    table.classList.toggle('d-none', isHidden);
+    btnShow.innerHTML = isHidden
+        ? `Mostra <i class="bi bi-eye-fill"></i>`
+        : `Nascondi <i class="bi bi-eye-slash-fill"></i>`;
+});
+
+// Avvio
+rubrica.showContacts();
+rubrica.totContacts();
+
+
 
